224f89f36b419b4b6845018610f3237e8f0dfa16
Emerson Dantas <emerson.dantaspereira@hotmail.com>
2025-05-29 23:50:37 -0300

    feat(infra/interfaces): create configs test e2e and run server

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,9 +1,40 @@
+import fastifyCookie from '@fastify/cookie'
+import fastifyCors from '@fastify/cors'
 import fastifyJwt from '@fastify/jwt'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUi from '@fastify/swagger-ui'
 import fastify from 'fastify'
+import {
+	type ZodTypeProvider,
+	jsonSchemaTransform,
+	serializerCompiler,
+	validatorCompiler,
+} from 'fastify-type-provider-zod'
 import { ZodError } from 'zod'
+import { administratorRoutes } from '../../interfaces/http/routes/administrator-routes'
+import { companyRoutes } from '../../interfaces/http/routes/company-routes'
 import { env } from '../config'
 
-export const app = fastify()
+export const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(fastifyCors, { origin: '*' })
+
+app.register(fastifySwagger, {
+	openapi: {
+		info: {
+			title: 'API Documentation',
+			version: '1.0.0',
+		},
+	},
+	transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUi, {
+	routePrefix: '/docs',
+})
 
 app.register(fastifyJwt, {
 	secret: env.JWT_SECRET,
@@ -16,6 +47,11 @@ app.register(fastifyJwt, {
 	},
 })
 
+app.register(fastifyCookie)
+
+app.register(companyRoutes)
+app.register(administratorRoutes)
+
 app.setErrorHandler((error, _, reply) => {
 	if (error instanceof ZodError) {
 		return reply
@@ -23,7 +59,7 @@ app.setErrorHandler((error, _, reply) => {
 			.send({ message: 'Validation error.', issue: error.format() })
 	}
 
-	if (env.NODE_ENV !== 'production') {
+	if (env.NODE_ENV !== 'prod') {
 		console.error(error)
 	} else {
 		// fazer um log para ferramenta interna.
