e9b30d8afd524332436364d632878d043835a054
Emerson Dantas <emerson.dantaspereira@hotmail.com>
2025-06-01 21:54:44 -0300

    fix: remove from the test coverage folders and configuration files

--- src/interfaces/http/controllers/company/delete-interviewer.ts
+++ src/interfaces/http/controllers/company/delete-interviewer.ts
@@ -1,13 +1,13 @@
+import { DeleteInterviewerUseCase } from '@/domain/company/application/use-cases/delete-interviewer'
+import type { DeleteInterviewerParams } from '@/domain/company/application/validators/delete-interviewer.schema'
+import { PrismaInterviewersRepository } from '@/infra/database/repositories/prisma-interviewers-repository'
 import type { FastifyReply, FastifyRequest } from 'fastify'
-import { DeleteInterviewerUseCase } from '../../../../domain/company/application/use-cases/delete-interviewer'
-import type { DeleteInterviewerSchema } from '../../../../domain/company/application/validators/delete-interviewer.schema'
-import { PrismaInterviewersRepository } from '../../../../infra/database/repositories/prisma-interviewers-repository'
 
 export async function deleteInterviewer(
 	request: FastifyRequest,
 	reply: FastifyReply,
 ) {
-	const { companyId, interviewerId } = request.body as DeleteInterviewerSchema
+	const { companyId, interviewerId } = request.params as DeleteInterviewerParams
 
 	const prismaInterviewersRepository = new PrismaInterviewersRepository()
 	const deleteInterviewerUseCase = new DeleteInterviewerUseCase(
