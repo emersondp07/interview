e00973a1bed70c008f85a36bc76ff184fa0147b1
Emerson Dantas Pereira <emerson.dantaspereira@hotmail.com>
2025-08-14 20:39:31 -0300

    fix: error in add columns in routes

--- src/application/administrator/validators/register-company.schema.ts
+++ src/application/administrator/validators/register-company.schema.ts
@@ -4,12 +4,7 @@ export const registerCompanySchema = z.object({
 	corporateReason: z.string().min(1, 'Corporate reason is required'),
 	cnpj: z.string().min(11, 'CNPJ is required'),
 	email: z.string().email('Invalid email format'),
-	password: z
-		.string()
-		.min(8, 'A senha deve conter pelo menos 8 caracteres')
-		.regex(/[A-Z]/, 'Deve conter uma letra maiúscula')
-		.regex(/[a-z]/, 'Deve conter uma letra minúscula')
-		.regex(/[0-9]/, 'Deve conter um número'),
+	password: z.string().min(8, 'Password must be at least 8 characters long'),
 	phone: z.string().min(1, 'Phone is required'),
 	planId: z.string().uuid('Plan ID is required'),
 })
